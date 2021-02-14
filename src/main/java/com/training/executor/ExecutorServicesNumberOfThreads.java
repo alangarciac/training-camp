09bc3623f298fb1cd79b78e062009c722206705a
alangarciac <alangarciacamina@gmail.com>
2021-02-13 22:10:15 -0300

    commit over remote

---
+++ src/main/java/com/training/executor/ExecutorServicesNumberOfThreads.java
@@ -0,0 +1,54 @@
+package com.training.executor;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExecutorServicesNumberOfThreads {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Log("Available processors: " + Runtime.getRuntime().availableProcessors());
+        Log("Parallelism of your CPU: " + ForkJoinPool.commonPool().getParallelism());
+
+        List<ExecutorService> executorServices = Arrays.asList(
+                //Executors.newCachedThreadPool(),
+                Executors.newFixedThreadPool(10)
+                //Executors.newSingleThreadExecutor(),
+                //Executors.newWorkStealingPool(),
+                //Executors.newScheduledThreadPool(5),
+                //ForkJoinPool.commonPool()
+        );
+
+        List<Callable<Object>> tasks = Stream.generate(ExecutorServicesNumberOfThreads::getTaskSleepUnSegundo)
+                .limit(100)
+                .collect(Collectors.toList());
+
+        for (ExecutorService executorService : executorServices) {
+            Instant start = Instant.now();
+            executorService.invokeAll(tasks);
+            Log("time:" + Duration.between(start, Instant.now()));
+        }
+
+        executorServices.stream().forEach(ExecutorService::shutdown);
+    }
+
+    private static Callable<Object> getTaskSleepUnSegundo() {
+        return Executors.callable(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static void Log(Object message) {
+        System.out.println(String.format("%s", message.toString()));
+    }
+
+}
---
+++ src/main/java/com/training/executor/TrainingCampExecutorService.java
@@ -0,0 +1,85 @@
+package com.training.executor;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
+
+public class TrainingCampExecutorService {
+
+    private static final Instant INIT = Instant.now();
+
+    public static void main(String[] args) {
+
+        threadTest();
+        //executorSingleThreadTest();
+        //executorCallableTest();
+
+    }
+
+    private static void executorCallableTest() {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> futureTask1 = executorService.submit(getCallableTask("1"));
+
+        try {
+            Log(futureTask1.isDone());
+            String result = futureTask1.get();
+            Log(futureTask1.isDone());
+            Log(result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executorService.shutdown();
+    }
+
+    private static void executorSingleThreadTest() {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(getRunnableTask("1"));
+        executorService.submit(getRunnableTask("2"));
+        executorService.shutdown();
+
+    }
+
+    private static void threadTest() {
+
+   
+    }
+
+    private static Runnable getRunnableTask(String number) {
+        return () -> {
+                try {
+                    Log("Start task " + number);
+                    TimeUnit.SECONDS.sleep(3);
+                    Log("End task " + number);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+    }
+
+    private static Callable<String> getCallableTask(String s) {
+        return ()->{
+                Log("Start task: " + s);
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log("End task: " + s);
+                return ("Result of task:  " + s);
+        };
+    }
+
+    private static void Log(Object message) {
+        System.out.println(String.format("%s [%s] %s",
+                Duration.between(INIT, Instant.now()), Thread.currentThread().getName(), message.toString()));
+    }
+
+}
